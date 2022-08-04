d9b7999c137c6c6263011cf6ea11c0593ca22e88
tegic <maksimkruglik2@gmail.com>
2022-08-04 20:31:47 +0200

    add new TODO

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import game
 
 pygame.init()
 
+# TODO: fix bug with "Start game" button (now you can start game with ships count, that less, that max)
 
 def main():
     player_1, player_2 = 0, 1
