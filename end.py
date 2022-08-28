0849d2b0971bec5bcd221d5e674e406154eb2e4a
R4zar0x <legosi057@gmail.com>
2022-08-28 23:58:04 +0300

    added restart button

--- end.py
+++ end.py
@@ -3,20 +3,26 @@ import pygame
 import config as cfg
 
 
-def events(button):
+def events(buttons):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             if cfg.end:
                 cfg.end = False
                 cfg.run = False
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            if button.is_mouse_on_button():
-                button.button_click()
+            for button in buttons:
+                if button.is_mouse_on_button():
+                    button.button_click()
 
 
-def button_end():
+def button_exit():
     cfg.end = False
     cfg.run = False
+
+
+def button_restart():
+    cfg.end = False
+    cfg.menu = True
 
 
 def draw_text(surface, x, y, text, text_color=pygame.Color("lightblue")):
--- main.py
+++ main.py
@@ -27,111 +27,120 @@ def show_game_mode():
 
 
 def main():
-    player_1, player_2 = 0, 1
-    grid_1 = Grid(player_1, player_2, "Invoker")
-    grid_2 = Grid(player_2, player_1, "Axe")
-    button = Button(300, 180, 100, 50)
-    button.set_text("Next player")
-    button.set_function(menu.button_next, grid_1)
+    while cfg.run:
+        player_1, player_2 = 0, 1
+        grid_1 = Grid(player_1, player_2, "Invoker")
+        grid_2 = Grid(player_2, player_1, "Axe")
+        button = Button(300, 180, 100, 50)
+        button.set_text("Next player")
+        button.set_function(menu.button_next, grid_1)
 
-    box = Box(30, 260, 140, 32)
-    box.set_text(grid_1.get_player_name())
+        box = Box(30, 260, 140, 32)
+        box.set_text(grid_1.get_player_name())
 
-    cfg.game_mode = cfg.game_mods[0]
+        cfg.game_mode = cfg.game_mods[0]
 
-    while cfg.menu and cfg.run:
-        # events
-        menu.events(grid_1, button, box, 300, 50)
+        while cfg.menu and cfg.run:
+            # events
+            menu.events(grid_1, button, box, 300, 50)
 
-        button.set_button_color(menu.button_color(grid_1))
-        button.set_text_color(menu.button_color(grid_1))
+            button.set_button_color(menu.button_color(grid_1))
+            button.set_text_color(menu.button_color(grid_1))
 
-        box.update()
+            box.update()
 
-        # draw functions
-        # screen.fill(pygame.Color("white"))
-        screen.blit(cfg.background, (0, 0))
+            # draw functions
+            # screen.fill(pygame.Color("white"))
+            screen.blit(cfg.background, (0, 0))
 
-        grid_1.draw_grid(screen, 10, 10, player=0)
+            grid_1.draw_grid(screen, 10, 10, player=0)
 
-        constructor.draw_constructor(grid_1, 300, 50)
-        button.draw_button(screen)
-        box.draw(screen)
+            constructor.draw_constructor(grid_1, 300, 50)
+            button.draw_button(screen)
+            box.draw(screen)
 
-        menu.text_draw(screen, grid_1, 30, 240)
+            menu.text_draw(screen, grid_1, 30, 240)
 
-        clock.tick(fps)
-        pygame.display.flip()
+            clock.tick(fps)
+            pygame.display.flip()
 
-    grid_1.set_player_name(box.get_text())
-    box.set_text(grid_2.get_player_name())
+        grid_1.set_player_name(box.get_text())
+        box.set_text(grid_2.get_player_name())
 
-    button.set_text("Start game")
-    button.set_function(menu.button_next, grid_2)
+        button.set_text("Start game")
+        button.set_function(menu.button_next, grid_2)
 
-    cfg.menu = True
-    while cfg.menu and cfg.run:
-        # events
-        menu.events(grid_2, button, box, 300, 300)
+        cfg.menu = True
+        while cfg.menu and cfg.run:
+            # events
+            menu.events(grid_2, button, box, 300, 300)
 
-        button.set_button_color(menu.button_color(grid_2))
-        button.set_text_color(menu.button_color(grid_2))
+            button.set_button_color(menu.button_color(grid_2))
+            button.set_text_color(menu.button_color(grid_2))
 
-        box.update()
+            box.update()
 
-        # draw functions
-        # screen.fill(pygame.Color("white"))
-        screen.blit(cfg.background, (0, 0))
+            # draw functions
+            # screen.fill(pygame.Color("white"))
+            screen.blit(cfg.background, (0, 0))
 
-        grid_2.draw_grid(screen, 10, 10, player=1)
+            grid_2.draw_grid(screen, 10, 10, player=1)
 
-        constructor.draw_constructor(grid_2, 300, 50)
-        button.draw_button(screen)
-        box.draw(screen)
+            constructor.draw_constructor(grid_2, 300, 50)
+            button.draw_button(screen)
+            box.draw(screen)
 
-        clock.tick(fps)
-        pygame.display.flip()
+            clock.tick(fps)
+            pygame.display.flip()
 
-    grid_2.set_player_name(box.get_text())
+        grid_2.set_player_name(box.get_text())
 
-    cfg.game_mode = cfg.game_mods[2]
-    cfg.game = True
-    while cfg.game and cfg.run:
-        # screen.fill(pygame.Color("white"))
-        screen.blit(cfg.background, (0, 0))
+        cfg.game_mode = cfg.game_mods[2]
+        cfg.game = True
+        while cfg.game and cfg.run:
+            # screen.fill(pygame.Color("white"))
+            screen.blit(cfg.background, (0, 0))
 
-        grid_1.draw_grid(screen, 10, 10, player=0)
-        grid_2.draw_grid(screen, 10, 300, player=1)
-        grid_2.draw_grid(screen, 250, 10, player=0)
-        grid_1.draw_grid(screen, 250, 300, player=1)
+            grid_1.draw_grid(screen, 10, 10, player=0)
+            grid_2.draw_grid(screen, 10, 300, player=1)
+            grid_2.draw_grid(screen, 250, 10, player=0)
+            grid_1.draw_grid(screen, 250, 300, player=1)
 
-        game.events([grid_1, grid_2])
+            game.events([grid_1, grid_2])
 
-        clock.tick(fps)
-        pygame.display.flip()
+            clock.tick(fps)
+            pygame.display.flip()
 
-    button = Button(cfg.screen_width / 2 - 50, 200, 100, 50)
-    button.set_text("Exit")
-    button.set_button_color(pygame.Color("lightblue"))
-    button.set_text_color(pygame.Color("lightblue"))
+        button_exit = Button(cfg.screen_width / 2 - 50, 200, 100, 50)
+        button_exit.set_text("Exit")
+        button_exit.set_button_color(pygame.Color("lightblue"))
+        button_exit.set_text_color(pygame.Color("lightblue"))
+        button_exit.set_function(end.button_exit, None)
 
-    button.set_function(end.button_end, None)
+        button_restart = Button(cfg.screen_width / 2 - 50, 260, 100, 50)
+        button_restart.set_text("Restart")
+        button_restart.set_button_color(pygame.Color("lightblue"))
+        button_restart.set_text_color(pygame.Color("lightblue"))
+        button_restart.set_function(end.button_restart, None)
 
-    cfg.end = True
-    while cfg.end and cfg.run:
-        end.events(button)
+        buttons = [button_exit, button_restart]
 
-        # screen.fill(pygame.Color("white"))
-        screen.blit(cfg.background, (0, 0))
+        cfg.end = True
+        while cfg.end and cfg.run:
+            end.events(buttons)
 
-        end.draw_text(screen,
-                      (cfg.screen_width / 2), 150,
-                      f"{cfg.winner} win")
+            # screen.fill(pygame.Color("white"))
+            screen.blit(cfg.background, (0, 0))
 
-        button.draw_button(screen)
+            end.draw_text(screen,
+                          (cfg.screen_width / 2), 150,
+                          f"{cfg.winner} win")
 
-        clock.tick(fps)
-        pygame.display.flip()
+            for button in buttons:
+                button.draw_button(screen)
+
+            clock.tick(fps)
+            pygame.display.flip()
 
 if __name__ == "__main__":
     main()
